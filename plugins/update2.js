a0403a01e1ae45d71039c6b51b47f5fffb1e43a0
Rhishna Capital <85033473+naisayuriza@users.noreply.github.com>
2021-06-28 16:26:06 +0700

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const { join } = require('path')
 
 let confirmation = {}
-let repository = 'Nurutomo/wabot-aq'
+let repository = 'naisayuriza/naisabotz'
 let branch = 'master'
 
 async function handler(m, { text }) {
